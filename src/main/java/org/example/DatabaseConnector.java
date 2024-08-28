a0eb616c72d952faffaa607a8683b4850cb75d4a
EastonA01 <eastonredwing@gmail.com>
2024-08-28 11:17:42 -0400

    Satisfied requirements AND added additional methods

--- src/main/java/org/example/DatabaseConnector.java
+++ src/main/java/org/example/DatabaseConnector.java
@@ -1,2 +1,80 @@
-package org.example;public class DatabaseConnector {
+package org.example;
+
+import java.sql.*;
+
+public class DatabaseConnector {
+    public Connection connect() {
+        Connection connection = null;
+        try {
+            String url = "jdbc:sqlite:C:\\Users\\admin\\Desktop\\CTAC-DB-Practice\\CTAC-104-SQL-Mastery.db";
+            String username = "yourUsername";
+            String password = "yourPassword";
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Successfully connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void selectAllBooks() {
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            connection = this.connect();
+            String sql = "SELECT * FROM books";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("title") + "\t" +
+                        rs.getString("author") + "\t" +
+                        rs.getDouble("price"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error executing SELECT statement");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void selectAllSales(){
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            connection = this.connect();
+            String sql = "SELECT * FROM sales";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("saleid") + "\t" +
+                        rs.getString("bookid") + "\t" +
+                        rs.getDouble("saledate")+ "\t" +
+                        rs.getDouble("quantity")+ "\t" +
+                        rs.getDouble("totalprice"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error executing SELECT statement");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
